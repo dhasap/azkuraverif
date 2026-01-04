96e59a10dfdc54d3a0064e94b2c0347f98d6eda5
ds4217440 <ds4217440@gmail.com>
2026-01-04 12:55:38 +0000

    fix error youtube

--- services/youtube/config.py
+++ services/youtube/config.py
@@ -1,9 +1,34 @@
 # YouTube Config
 from services.utils.universities import get_weighted_university
 
-PROGRAM_ID = '67c8c14f5f17a83b745e3f82' # YouTube Student Program ID (Placeholder - usually dynamic)
+# Program ID extracted from YouTube Student SheerID Page
+PROGRAM_ID = '633f45d7295c0551ab43b87a' 
 SHEERID_BASE_URL = 'https://services.sheerid.com'
 MAX_FILE_SIZE = 1 * 1024 * 1024
+
+# Default School Configuration
+DEFAULT_SCHOOL_ID = '21415' # PSU
+
+SCHOOLS = {
+    '21415': {
+        'id': 21415,
+        'idExtended': '21415',
+        'name': 'Pennsylvania State University',
+        'domain': 'psu.edu'
+    },
+    '20688': {
+        'id': 20688,
+        'idExtended': '20688',
+        'name': 'Ohio State University',
+        'domain': 'osu.edu'
+    },
+    '23068': {
+        'id': 23068,
+        'idExtended': '23068',
+        'name': 'University of Southern California',
+        'domain': 'usc.edu'
+    }
+}
 
 def get_random_school():
     return get_weighted_university()
